01c6f77c70681847dc86f96617ab37bf572c5f4b
miguel_casas <miguel.casas@atmira.com>
2020-05-21 19:52:24 +0200

    hasta pipe json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  nombre: string = 'Capitán América';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.235;
+
+  salario: number = 1234.5;
+
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
 }
